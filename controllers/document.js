33780ea9759228a505eee9c9a20221cc27206fe4
Nikhil <nikhilsunil90s@gmail.com>
2024-01-15 11:52:40 +0530

    document controllers

---
+++ controllers/document.js
@@ -0,0 +1,113 @@
+const DocumentModel = require("../models/document");
+const fs = require("fs");
+const { json } = require("body-parser");
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({ 
+    cloud_name: 'dgejssmll', 
+    api_key: '235636159233983', 
+    api_secret: 'eXjWXEfK79rDnd47QXcZ9TvZymc' 
+});
+
+
+async function uploadToCloudinary(locaFilePath) {
+    // locaFilePath :
+    // path of image which was just uploaded to "uploads" folder
+  
+    var mainFolderName = "uploads"
+    // filePathOnCloudinary :
+    // path of image we want when it is uploded to cloudinary
+    var filePathOnCloudinary = mainFolderName + "/" + locaFilePath
+    
+    return cloudinary.uploader.upload(filePathOnCloudinary, {"public_id": locaFilePath, "resource_type": "auto"})
+    .then((result) => {
+      // Image has been successfully uploaded on cloudinary
+      // So we dont need local image file anymore
+      // Remove file from local uploads folder 
+    //   fs.unlinkSync(filePathOnCloudinary);
+      //console.log(result);
+      return {
+        message: "Success",
+        url:result.url,
+        public_id: result.public_id,
+      };
+    }).catch((error) => {
+        //console.log(error);
+      // Remove file from local uploads folder 
+    //   fs.unlinkSync(filePathOnCloudinary)
+      return {message: "Fail",};
+    });
+}
+
+
+exports.uploadDocument = async (req, res, next) => {
+    const {documentTitle} = req.body;
+    const documentFilePath = req.file.filename;
+
+    var result = await uploadToCloudinary(documentFilePath);
+    let today = new Date();
+    let newDocument = new DocumentModel({
+        docName: documentTitle,
+        docUrl: result.url,
+        docGeneratedOn: today.toISOString(),
+        publicId: result.public_id
+    })
+
+    newDocument.save().then(saved => {
+        return res.status(200).json({
+            status: true,
+            message: `${documentTitle} Uploaded Successfully!`,
+            fileUrl: result.url
+        })
+    }).catch(err => {
+        console.log(err);
+        return res.status(400).json({
+            status: false,
+            message: `${documentTitle} Uploade Failed, Please Try Again!`,
+        })
+    })
+}
+
+exports.deleteDocument = async (req, res, next) => {
+    const { documentId, publicId } = req.body;
+
+    cloudinary.uploader.destroy(publicId,  { invalidate: true, resource_type: "raw" }, (result) => {
+        //console.log(result);
+        fs.unlinkSync('uploads/' + publicId);
+    });
+
+    await DocumentModel.findByIdAndDelete(documentId).then(deleted => {
+        return res.status(200).json({
+            status: true,
+            message: `${publicId} Deleted Successfully!`
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(400).json({
+            status: false,
+            message: `${publicId} Delete Failed, Please Try Again!`
+        })
+    })
+}
+
+exports.getAllDocuments = async (req, res, next) => {
+    let { type } = req.query; // all, signed, unsigned
+
+    let query = {};
+
+    if (type === 'all') {
+        query = {}
+    } else if (type === 'signed') {
+        query = { signature: { $exists: true, $ne: null } }
+    } else {
+        query = { signature: { $exists: false } }
+    }
+
+    let results = await DocumentModel.find(query).sort({ documentGeneratedOn: -1 }).exec();
+    return res.status(200).json({
+        status: true,
+        total: results.length,
+        data: results
+    })
+}
--- routes/routes/document.js
+++ routes/routes/document.js
@@ -9,14 +9,15 @@ var storage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         ////console.log('file-', file)
-        cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname))
+        cb(null, req.body.documentTitle + "-" + Date.now() + path.extname(file.originalname))
     }
 });
 
 var upload = multer({ storage: storage });
 
+router.get("/getAllDocuments", DocumentController.getAllDocuments);
 router.post("/uploadDocument" , upload.single('document'), DocumentController.uploadDocument);
-router.get("/deleteDocument", DocumentController.deleteDocument);
+router.post("/deleteDocument", DocumentController.deleteDocument);
 
 module.exports = {
     router: router,
